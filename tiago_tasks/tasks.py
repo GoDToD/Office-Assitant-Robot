89361eca5c548a0850005bf02858a1ffbd1e5165
ZHU YINGE <zhuyinge18@gmail.com>
2025-05-02 08:22:46 +0800

    task

--- tiago_tasks/tasks.py
+++ tiago_tasks/tasks.py
@@ -1,9 +1,12 @@
 import rclpy
 from rclpy.node import Node
+from rclpy.action import ActionClient
 from geometry_msgs.msg import PoseStamped
 from nav2_simple_commander.robot_navigator import BasicNavigator, TaskResult
 import math
 import time
+from control_msgs.action import FollowJointTrajectory
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 
 from std_msgs.msg import Float64
 #from moveit_commander import MoveGroupCommander
@@ -66,7 +69,7 @@ class GoToPose(Node):
         result = self.navigator.getResult()
         if result == TaskResult.SUCCEEDED:
             self.get_logger().info("✅ Navigation Success!")
-            self.grab_coke_can()  # 🎯 抵达目标后开始抓取
+            #self.grab_coke_can()  # 🎯 抵达目标后开始抓取
         else:
             self.get_logger().warn(f"❌ Navigation Failed: {result}")
 
@@ -184,11 +187,141 @@ class GoToPose(Node):
     #     arm_group.stop()
     #     arm_group.clear_pose_targets()
     #     self.get_logger().info("✅ Coke can grabbed successfully!")
+class ArmController(Node):
+    def __init__(self):
+        super().__init__('arm_trajectory_controller')
+        self._client = ActionClient(self, FollowJointTrajectory, '/arm_controller/follow_joint_trajectory')
+
+    def send_target_pose(self,joints):
+        self.get_logger().info('⏳ Waiting for arm action server...')
+        self._client.wait_for_server()
+
+        goal_msg = FollowJointTrajectory.Goal()
+        goal_msg.trajectory.joint_names = [
+            'arm_1_joint',
+            'arm_2_joint',
+            'arm_3_joint',
+            'arm_4_joint',
+            'arm_5_joint',
+            'arm_6_joint',
+            'arm_7_joint'
+        ]
+
+        # 角度 → 弧度
+        def deg(rad): return math.radians(rad)
+
+        point = JointTrajectoryPoint()
+        point.positions = [
+            deg(joints[0]),
+            deg(joints[1]),
+            deg(joints[2]),
+            deg(joints[3]),
+            deg(joints[4]),
+            deg(joints[5]),
+            deg(joints[6])
+        ]
+        point.time_from_start.sec = 4
+        goal_msg.trajectory.points = [point]
+
+        self.get_logger().info('🎯 Sending joint trajectory goal...')
+        send_goal_future = self._client.send_goal_async(goal_msg)
+        rclpy.spin_until_future_complete(self, send_goal_future)
+
+        goal_handle = send_goal_future.result()
+        if not goal_handle.accepted:
+            self.get_logger().error('❌ Goal was rejected by arm controller')
+            return
+
+        self.get_logger().info('✅ Goal accepted. Executing...')
+        result_future = goal_handle.get_result_async()
+        rclpy.spin_until_future_complete(self, result_future)
+        self.get_logger().info('✅ Trajectory execution done!')
+
+class GripperController(Node):
+    def __init__(self):
+        super().__init__('gripper_action_controller')
+        self._client = ActionClient(self, FollowJointTrajectory, '/gripper_controller/follow_joint_trajectory')
+
+    def send_gripper_goal(self, position: float):
+        self.get_logger().info('⏳ Waiting for gripper action server...')
+        self._client.wait_for_server()
+
+        goal_msg = FollowJointTrajectory.Goal()
+        goal_msg.trajectory.joint_names = [
+            'gripper_right_finger_joint',
+            'gripper_left_finger_joint'
+        ]
+
+        point = JointTrajectoryPoint()
+        point.positions = [position, position]
+        point.time_from_start.sec = 4
+        goal_msg.trajectory.points = [point]
+
+        self.get_logger().info(f'🚀 Sending goal: position = {position}')
+        send_goal_future = self._client.send_goal_async(goal_msg)
+        rclpy.spin_until_future_complete(self, send_goal_future)
+
+        goal_handle = send_goal_future.result()
+        if not goal_handle.accepted:
+            self.get_logger().error('❌ Goal was rejected by gripper controller')
+            return
+
+        self.get_logger().info('✅ Goal accepted. Waiting for result...')
+        result_future = goal_handle.get_result_async()
+        rclpy.spin_until_future_complete(self, result_future)
+        result = result_future.result()
+
+        if result.result.error_code == 0:
+            self.get_logger().info('✅ Gripper action succeeded!')
+        else:
+            self.get_logger().error(f'❌ Gripper action failed with error code: {result.error_code}')
+def test(args):
+    rclpy.init(args=args)
+    node1 = ArmController()
+    node2 = GripperController()
+    node1.send_target_pose([55,0,-90,100,0,0,0])
+    time.sleep(4)
+    node2.send_gripper_goal(0.03)
+    time.sleep(4)
+    node1.send_target_pose([55,20,-90,100,0,0,0])
+    node1.destroy_node()
+    node2.destroy_node()
+    
+    rclpy.shutdown()
 def main(args=None):
+    #test(args)
     rclpy.init(args=args)
     node = GoToPose()
     rclpy.spin_once(node)
     node.destroy_node()
+    time.sleep(4)
+
+    node1 = ArmController()
+    node1.send_target_pose([0,0,0,0,0,0,0])
+    time.sleep(4)
+    node1.send_target_pose([55,0,0,0,0,0,0])
+    time.sleep(4)
+    node1.send_target_pose([55,30,0,0,0,0,0])
+    time.sleep(4)
+    node1.send_target_pose([55,30,-90,0,0,0,0])
+    time.sleep(4)
+    node1.send_target_pose([55,30,-90,100,0,0,0])
+    time.sleep(4)
+
+    node2 = GripperController()
+    # 🖐️ 打开夹爪（0.05）
+    node2.send_gripper_goal(0.05)
+    time.sleep(4)
+    node1.send_target_pose([55,0,-90,100,0,0,0])
+    time.sleep(4)
+    # ✊ 关闭夹爪（0.035）
+    node2.send_gripper_goal(0.03)
+    time.sleep(4)
+    node1.send_target_pose([55,20,-90,100,0,0,0])
+
+    node1.destroy_node()
+    node2.destroy_node()
+
     rclpy.shutdown()
 
 if __name__ == '__main__':
